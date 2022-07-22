b56af75e63e016865676d65b1673c7c4382d9513
heogeon0 <heo3793@naver.com>
2022-07-22 16:31:43 +0900

    [FE] #11-3-1 createChildern

--- frontend-web/src/Router.js
+++ frontend-web/src/Router.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./router/SignUp";
 import Login from "./router/Login";
 import Intro from "./router/Intro";
+import Main from "./router/Main";
+import CreateChildern from "./router/CreateChildern/Index";
 
 function Router() {
   return (
@@ -13,7 +15,9 @@ function Router() {
           {/* <Route path="/"></Route> */}
           <Route path="/signup" element={<SignUp />}></Route>
           <Route path="/login" element={<Login />}></Route>
-          <Route path="/intro" element={<Intro />}></Route>
+          <Route path="/" element={<Intro />}></Route>
+          <Route path="/main" element={<Main />}></Route>
+          <Route path="/createChildern" element={<CreateChildern />}></Route>
         </Routes>
       </BrowserRouter>
     </>
