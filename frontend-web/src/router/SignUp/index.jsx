15f9222845f29a7e704d8b0b20bfa84915c89c48
heogeon0 <heo3793@naver.com>
2022-07-22 15:09:49 +0900

    [FE] #11-2-2 Signup Design

--- frontend-web/src/router/SignUp/index.jsx
+++ frontend-web/src/router/SignUp/index.jsx
@@ -1,6 +1,17 @@
+import Wrapper from "./styles";
+
+import SignUpForm from "../../components/Member/SignUpForm";
+
 function SignUp() {
-
-
+  return (
+    <>
+      <Wrapper>
+        <div className="title">
+          <h3>회원가입</h3>
+        </div>
+        <SignUpForm />
+      </Wrapper>
+    </>
+  );
 }
-
 export default SignUp;
