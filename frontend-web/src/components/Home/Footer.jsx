e584943a8158af6cc2b31b2a03063907b5d56724
jeong57 <wjddlsp0507@naver.com>
2022-07-28 09:51:20 +0900

    [FE] #3-1-2 feat:footer v2

--- frontend-web/src/components/Home/Footer.jsx
+++ frontend-web/src/components/Home/Footer.jsx
@@ -1,21 +1,28 @@
 import styled from "styled-components";
 
 const Box = styled.div`
-  position: fixed;
-  bottom: 15px;
+  position: relative;
+  bottom: 0px;
+  padding: 1rem 2rem 1rem;
   width: 100%;
+  /* height: 110px; */
 `
 
 const FooterText = styled.div`
-  text-align: center;
 `
 
 function Footer () {
   return (
     // <div>
       <Box>
-        <FooterText>SSAFY 7th Class2 of Gumi | Design By D208</FooterText>
-        <FooterText>주소: 경상북도 구미시 임수동 94</FooterText>
+        <FooterText>
+          <a style={{textDecoration: 'none'}} href="https://jira.ssafy.com/secure/RapidBoard.jspa?rapidView=12725&projectKey=S07P12D208&selectedIssue=S07P12D208-92&quickFilter=26527">Jira</a>
+          <span> | </span>
+          <a style={{textDecoration: 'none'}} href="https://lab.ssafy.com/s07-webmobile3-sub2/S07P12D208">Gitlab</a>
+          </FooterText>
+        <FooterText>제작: 허건녕 김민정 박신혜 오도석 최연지</FooterText>
+        <FooterText>이메일: ssafy@naver.com</FooterText>
+        <FooterText>Copyright 2022. D208. All Rights Reserved.</FooterText>
       </Box>
     // </div>
   )
