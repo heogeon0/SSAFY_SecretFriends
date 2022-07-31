79d700d4815f37f22daa6ad7c654e68b8659f72f
heogeon0 <heo3793@naver.com>
2022-08-01 00:52:34 +0900

    [FE] #3-2-2_feat: Carousel

---
+++ frontend-web/src/components/Main/Slider.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const NowSlider = styled.div`
+  width: ${(props) => (props.check === "now" ? "30%" : "25%")};
+  padding-top: ${(props) => (props.check === "now" ? "30%" : "25%")};
+  border-radius: 50%;
+  transform: translate(-50%, -50%);
+  transition: all ease-in-out 0.5s;
+  position: absolute;
+  top: 50%;
+
+  left: ${(props) =>
+    (props.check === "now" && "50%") ||
+    (props.check === "next" && "80%") ||
+    (props.check === "prev" && "20%")};
+
+  background-image: url(${(props) => props.bg});
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+function Slider({ check, bg }) {
+  console.log(check);
+  return (
+    <>
+      {check !== "hidden" ? (
+        <NowSlider bg={bg} check={check}></NowSlider>
+      ) : null}
+    </>
+  );
+}
+
+export default Slider;
