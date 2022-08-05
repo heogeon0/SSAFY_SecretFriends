abd91bc37288a9651803f3518c146058edf29a9f
doseok <pkqwe37@gmail.com>
2022-08-05 11:07:05 +0900

    [BE] feat : 오류수정

--- backend-web/src/main/java/com/ssafy/mybuddy/dto/AnswerDto.java
+++ backend-web/src/main/java/com/ssafy/mybuddy/dto/AnswerDto.java
@@ -23,6 +23,6 @@ public class AnswerDto {
     @ApiModelProperty(value = "질문 사용 여부")
     private Boolean isUsed;
     @ApiModelProperty(value = "질문이 생성된 시간")
-    private String cratedAt;
+    private String createdAt;
 
 }
