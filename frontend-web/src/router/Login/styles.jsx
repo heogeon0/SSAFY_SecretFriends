75c3dbe2974e26228229336a6065696bf2231eaa
jeong57 <wjddlsp0507@naver.com>
2022-08-11 00:47:15 +0900

    [FE] #2-4 feat: form

--- frontend-web/src/router/Login/styles.jsx
+++ frontend-web/src/router/Login/styles.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  position: relative;
+  position: absolute;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
@@ -11,9 +11,10 @@ const Wrapper = styled.div`
   transform: translate(-50%, -50%);
   width: 80vw;
   height: calc(80vw / 1.3);
-  max-width: 500px;
+  max-width: 400px;
   max-height: calc(600px / 1.68);
   background-color: ${(props) => props.theme.whiteColor};
+  border-radius: 40px;
   box-shadow: 5px 5px 15px 0.5px #dcdde1;
   .title {
     font-family: ${(props) => props.theme.namingFont};
