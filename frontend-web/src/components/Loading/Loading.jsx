15b5e08c107f0392725fbf152c886958c16e2442
jeong57 <wjddlsp0507@naver.com>
2022-08-12 11:56:01 +0900

    [FE] #2-5 feat: loading page

--- frontend-web/src/components/Loading/Loading.jsx
+++ frontend-web/src/components/Loading/Loading.jsx
@@ -4,8 +4,9 @@ import { Background, LoadingText } from "./styles";
 function Loading() {
   return (
     <Background>
-      <LoadingText>잠시만 기다려 주세요.</LoadingText>
-      <i className="fa-solid fa-spinner fa-spin-pulse"></i>
+      {/* <LoadingText style={{fontSize: "min(5vw, 4rem)", marginBottom: "1rem", color: "#000000ac"}}>Page Loading</LoadingText>
+      <LoadingText style={{fontSize: "min(3.5vw, 2rem)", marginBottom: "1.5rem", color: "#0000006e"}}>잠시만 기다려 주세요.</LoadingText> */}
+      <i className="fa-solid fa-spinner fa-spin-pulse" style={{color: "gray", fontSize: "min(5vw, 3rem)"}}></i>
     </Background>
   )
 }
