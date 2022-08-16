e8e10f036c8f7a406ad86d3406db4f0fd30edd42
jeong57 <wjddlsp0507@naver.com>
2022-08-16 22:40:53 +0900

    [FE] #4-2-2 feat: chat css

--- frontend-web/src/components/chats/MyChats.jsx
+++ frontend-web/src/components/chats/MyChats.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
-import io from "socket.io-client";
 import styled from "styled-components";
-import { useSetRecoilState, useRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import { MemberID } from "../../atom";
 
 const Wrapper = styled.div`
@@ -21,6 +20,40 @@ const Wrapper = styled.div`
       border-radius: 5px;
       padding: 1vw;
       overflow-y: scroll;
+      .parent__chat {
+        font-family: ${props => props.theme.pretendard};
+        font-size: 16px;
+        text-align: right;
+        display: inline-block;
+
+        margin: 5px;
+        padding: 5px 8px;
+        border-radius: 5px;
+        box-shadow: 3px 3px 3px #dbdbdb;
+        background-color: #f7f3c1;
+        @media ${props => props.theme.mobile} {
+          font-size: min(1.3vw, 1rem);
+        }
+      }
+      .child__chat {
+        font-size: 16px;
+        display: inline-block;
+        font-family: ${props => props.theme.pretendard};
+
+        margin: 5px;
+        padding: 5px 8px;
+        border-radius: 5px;
+        box-shadow: 3px 3px 3px #dbdbdb;
+        background-color: #c3f7c1;
+        @media ${props => props.theme.mobile} {
+          font-size: min(1.3vw, 1rem);
+        }
+      }
+      .talk__time {
+        font-size: 4px;
+        margin-bottom: 5px;
+        color: gray;
+      }
     }
     .grid__input {
       width: 100%;
@@ -39,20 +72,19 @@ const Wrapper = styled.div`
       }
     }
   }
-`;
-
-const CloseBtn = styled.i`
-  position: absolute;
-  right: 15px;
-  border: none;
-  background-color:transparent;
-  font-size: 16px;
-  color: #04200496;
-  font-weight: bold;
-  :hover {
-    cursor: pointer;
+  .closeBtn {
+    position: absolute;
+    right: 15px;
+    border: none;
+    background-color:transparent;
+    font-size: 16px;
+    color: #0000005f;
+    font-weight: bold;
+    :hover {
+      cursor: pointer;
+    }
   }
-`
+`;
 
 const SendImg = styled.img`
   :hover {
@@ -60,65 +92,30 @@ const SendImg = styled.img`
   }
 `
 
-// 채팅 box
-const ChildContent = styled.div`
-  float: left;
-  font-size: min(1.5vw, 1rem);
-  font-family: ${props => props.theme.pretendard};
-  
-  margin: 5px;
-  padding: 5px 8px;
-  width: 70%;
-  border-radius: 5px;
-  box-shadow: 3px 3px 3px #dbdbdb;
-  background-color: #c3f7c1;
-  @media ${props => props.theme.mobile} {
-    font-size: min(1.3vw, 1rem);
-  }
-`
 
-const ParentContent = styled.div`
-  float: right;
-  font-family: ${props => props.theme.pretendard};
-  font-size: min(1.5vw, 1rem);
-  text-align: right;
-  
-  margin: 5px;
-  padding: 5px 8px;
-  width: 70%;
-  border-radius: 5px;
-  box-shadow: 3px 3px 3px #dbdbdb;
-  background-color: #f7f3c1;
-  @media ${props => props.theme.mobile} {
-    font-size: min(1.3vw, 1rem);
-  }
-`
-
-// const socket = io.connect("http://localhost:4000");
 function MyChats({ name, socket, setOpenChat, setAlarm }) {
-  // useEffect(() => {
-  //   socket.on("connect", function () {
-  //     console.log("소켓이 바뀝니다" + name);
-  //     name = JSON.parse(name);
-  //     socket.emit("newUser", name);
-  //   });
-  // }, []);
-
   const [chat, setChat] = useState([]);
   const [readyChat, setReadyChat] = useState({});
   const [message, setMessage] = useState("");
-  const [memberID, setMemberId] = useRecoilState(MemberID);
-  const [test, setTest] = useState([{type: 0, content: "1"}, {type:1, content: "2"}, {type: 0, content: "3"}]);
+  const memberID = useRecoilValue(MemberID);
+  const [test, setTest] = useState([
+    {type: 0, content: "1", day: getDate(new Date()), time: getTime(new Date())}, 
+    {type:1, content: "2", day: getDate(new Date()), time: getTime(new Date())}, 
+    {type: 0, content: "3", day: getDate(new Date()), time: getTime(new Date())}, 
+    {type: 1, content: "안녕하세요", day: getDate(new Date()), time: getTime(new Date())}
+  ]);
 
   const sendMessageHandler = (e) => {
     e.preventDefault();
     // 수정해야할 부분
     const messageToken = {
       // memberId 를 다른 키로
-      memberID: 8,
+      memberID: memberID,
       message: message,
     };
-    setTest((val) => [...val, message])
+    // test용
+    setTest((val) => [...val, {type: 0, content: message, day: getDate(new Date()), time: getTime(new Date())}])
+    console.log(test)
     socket.emit("chat message", messageToken);
     setMessage("");
   };
@@ -131,8 +128,8 @@ function MyChats({ name, socket, setOpenChat, setAlarm }) {
       if (msg?.message !== readyChat?.message) {
         setReadyChat(msg);
       }
-
-      setAlarm("#f37e91")
+      // 아이 소켓 접속여부 확인
+      setAlarm("#f37e91");
     });
     socket.on("chat message", (msg) => {
       console.log(msg?.message);
@@ -157,17 +154,52 @@ function MyChats({ name, socket, setOpenChat, setAlarm }) {
   }, [readyChat]);
 
 
+  function getDate (day) {
+    var year = day.getFullYear();
+    var month = ('0' + (day.getMonth() + 1)).slice(-2);
+    var day = ('0' + day.getDate()).slice(-2);
+    
+    var dateString = year + '-' + month  + '-' + day;
+    return dateString;
+  }
+  function getTime (day) {
+    var hours = ('0' + day.getHours()).slice(-2); 
+    var minutes = ('0' + day.getMinutes()).slice(-2);
+    var seconds = ('0' + day.getSeconds()).slice(-2); 
+
+    if (hours >= 12) {
+      var timeString = '오후 ' + hours + ':' + minutes;
+    }
+    else {
+      var timeString = '오전 ' + hours + ':' + minutes;
+    }
+    // var timeString = hours + ':' + minutes  + ':' + seconds;
+    return timeString;
+  }
+
+
   return (
     <Wrapper>
-      <CloseBtn onClick={() => setOpenChat((val) => !val)} className="fa-solid fa-xmark"></CloseBtn>
-      <CloseBtn onClick={() => setOpenChat((val) => !val)} className="fa-solid fa-xmark"></CloseBtn>
+      <img src="img/logo/gray_mixed.jpg" style={{position: "absolute", height: "8%", borderRadius: "25px"}} alt="" />
+      <i onClick={() => setOpenChat((val) => !val)} className="fa-solid fa-xmark closeBtn"></i>
       <div className="gridBox">
         <div className="grid__top">
           <div>
             {test.map((data, idx) => {
-              return (<div key={idx}>
-                {data.type === 1 ? <ParentContent>{data.content}</ParentContent> : <ChildContent>{data.content}</ChildContent>}
-              </div>)
+              return (
+                <div key={idx}>
+                  {data.type === 0
+                  ? <div style={{display: "flex", justifyContent: "flex-end", alignItems: "flex-end"}}>
+                      <span className="talk__time">{data.time}</span>
+                      <span className="parent__chat">{data.content}</span>
+                    </div>
+                  : <div style={{display: "flex", alignItems: "flex-end"}}>
+                      <span className="child__chat">{data.content}</span>
+                      <span className="talk__time">{data.time}</span>
+                    </div>
+                  }
+                </div>
+              )
             })}
           </div>
         </div>
